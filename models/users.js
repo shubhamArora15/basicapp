9323098be46bc6745886c4d2aba6e23b9f20d149
shubhamarora15 <ashubham.1994@gmail.com>
2017-10-12 12:47:15 +0530

    basic routing done

---
+++ models/users.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+var userSchema = mongoose.Schema({
+  name:{
+    type: Number
+  },
+  phone:{
+    type: String
+  },
+  email:{
+    type:String
+  }
+  password:{
+    type: String
+  },
+  address:{
+    type:String
+  }  
+});
+
+var userModel = mongoose.model('user',userSchema);
+module.exports = userModel
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,48 @@
 var express = require('express');
 var router = express.Router();
 
+var users = require("../models/users");
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+  /*
+
+    var userData = new users({
+      name: req.body.name,
+      phone: req.body.phone,
+      email: req.body.email,
+      password: req.body.password,
+      address: req.body.address
+    });
+
+      userData.save(function(err, data){
+        if(data){
+          res.send(data)
+        }else{
+        res.send(data)
+      }
+
+    });
+  */
+}).post('/login', function(req, res, next){
+
+    /*
+      users.findOne({email: req.body.email}, function(err, data){
+        if(data!=null){
+        res.send("successfully login")
+      }
+    })
+    */
+}).get('/verify', function(req, res, next){
+
+    /*
+      users.findOne({email: req.body.email}, function(err, data){
+        if(data!=null){
+        res.send("successfully login")
+      }
+    })
+    */
 });
 
 module.exports = router;
